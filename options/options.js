2f310148258b8b8012580722947b51a6b1cb6d50
Crowdbotics <team@crowdbotics.com>
2024-02-10 18:59:57 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "billowing-mud-46228",
+  url: "https://billowing-mud-46228.botics.co",
+  api: "https://billowing-mud-46228.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
